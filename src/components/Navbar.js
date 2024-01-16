8e774175e6b9b8daa7d1ce79d2dc5f483dfb2847
ali-sheikhali <ali.sheikhali1998@gmail.com>
2024-01-16 11:22:19 +0330

    Navbar file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars } from "react-icons/fa6";
+import { useState } from 'react';
+import { IoClose } from "react-icons/io5";
 
 function Navbar() {
+  const [bar, setBar] = useState(false)
+  const barHandleClick = ()=>{
+    setBar(!bar)
+  }
   return (
-    <div>hi</div>
+    <div className='bg-black text-white flex justify-around py-3'>
+      <div>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+      </div>
+      <div onClick={barHandleClick} className='mt-1 cursor-pointer font-bold text-xl'>
+         {bar ? <IoClose/> :  <FaBars />} 
+      </div>
+    </div>
   )
 }
 
